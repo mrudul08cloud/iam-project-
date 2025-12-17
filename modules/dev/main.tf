559d7331ec0f845a0ceb49eed62fd815aa4ffb9b
Mrudul Lole <mrudullole08@gmail.com>
2025-12-17 12:50:56 +0530

    Add IAM roles and policies for development environment

--- modules/dev/main.tf
+++ modules/dev/main.tf
@@ -1,1 +1,21 @@
-g
+resource "aws_iam_role" "dev_roles" {
+  for_each = var.roles
+  name     = each.key
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = each.value.service
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dev_roles" {
+  for_each   = var.roles
+  role       = aws_iam_role.dev_roles[each.key].name
+  policy_arn = each.value.policy_arn
+}
