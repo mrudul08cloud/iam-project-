48bad44051c25069685829415ee1ee109735810f
Mrudul Lole <mrudullole08@gmail.com>
2025-12-17 12:50:24 +0530

    Create variables.tf

---
+++ modules/dev/variables.tf
@@ -0,0 +1,33 @@
+variable "roles" {
+  type = map(object({
+    service    = string
+    policy_arn = string
+  }))
+
+  default = {
+    dev-ec2-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    }
+
+    dev-lambda-role = {
+      service    = "lambda.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AWSLambdaBasicExecutionRole"
+    }
+
+    dev-s3-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+    }
+
+    dev-cloudwatch-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+    }
+
+    dev-ecr-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    }
+  }
+}
