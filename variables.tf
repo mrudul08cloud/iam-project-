3a5a76b15ec111d2cff83b73e8daf98905f0eee5
Mrudul Lole <mrudullole08@gmail.com>
2025-12-17 17:36:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -53,6 +53,34 @@ dev-ec2-role = {
       service    = "ec2.amazonaws.com"
       policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 
+data-lambda-role = {
+      service    = "lambda.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+    }
+
+    # 2️⃣ S3 – data lake (raw / processed / curated data)
+    data-s3-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    }
+
+    # 3️⃣ Glue – ETL jobs, crawlers, catalog
+    data-glue-role = {
+      service    = "glue.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+    }
+
+    # 4️⃣ Athena – SQL queries on S3 data
+    data-athena-role = {
+      service    = "athena.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
+    }
+
+    # 5️⃣ Redshift – data warehouse analytics
+    data-redshift-role = {
+      service    = "redshift.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonRedshiftReadOnlyAccess"
+
 }
   }
 }
