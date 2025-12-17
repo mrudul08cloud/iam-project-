82b3743e495b17ae15d99326ffd69c1e8d4f26a2
Mrudul Lole <mrudullole08@gmail.com>
2025-12-17 17:34:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -29,5 +29,30 @@ variable "roles" {
       service    = "ec2.amazonaws.com"
       policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
     }
+dev-ec2-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+    }
+
+    dev-lambda-role = {
+      service    = "lambda.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+    }
+
+    dev-s3-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    }
+
+    dev-cloudwatch-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+    }
+
+    dev-ecr-role = {
+      service    = "ec2.amazonaws.com"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+
+}
   }
 }
