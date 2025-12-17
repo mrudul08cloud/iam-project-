f1dde8ffdecc47cab1442f5c9ea09596bd998b75
Mrudul Lole <mrudullole08@gmail.com>
2025-12-17 12:53:06 +0530

    Add IAM roles and policies for data roles

--- modules/data/main.tf
+++ modules/data/main.tf
@@ -1,1 +1,21 @@
-s
+resource "aws_iam_role" "data_roles" {
+  for_each = var.roles
+  name     = each.key
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = each.value.service
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "data_roles" {
+  for_each   = var.roles
+  role       = aws_iam_role.data_roles[each.key].name
+  policy_arn = each.value.policy_arn
+}
